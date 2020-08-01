f903eb54d168f1478b3a33281de40b69e9490943
erkamow <erkamafsin@afsteknik.com>
2020-08-01 11:53:23 -0400

    update happyB20

--- src/HelloB20.java
+++ src/HelloB20.java
@@ -4,5 +4,10 @@ public class HelloB20 {
         System.out.println("Hello B20, it is my pleasure to be with you!");
 
         // added a line
+
+        int[] nums = {1,2,3,4,5};
+        for(int i = 0; i <= 4; i++){
+            System.out.print(i+" ");
+        }
     }
 }
