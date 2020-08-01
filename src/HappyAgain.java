7e145bdc9d0acd04d63fb7dd6bfc3d13bd75f542
erkamow <erkamafsin@afsteknik.com>
2020-08-01 12:43:55 -0400

    update HappyAgain

--- src/HappyAgain.java
+++ src/HappyAgain.java
@@ -1,6 +1,17 @@
+import java.util.ArrayList;
+
 public class HappyAgain {
 
     public static void main(String[] args) {
         System.out.println("Erkam loves kubra");
+
+
+        ArrayList<Integer> list = new ArrayList<>();
+
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        System.out.println(list);
     }
 }
