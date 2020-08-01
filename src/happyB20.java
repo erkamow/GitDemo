3fcefb0846bb4038db6a52bb15c36d44f991b3d3
erkamow <erkamafsin@afsteknik.com>
2020-08-01 11:31:06 -0400

    BI_23 add login.java

---
+++ src/happyB20.java
@@ -0,0 +1,10 @@
+public class happyB20 {
+    public static void main(String[] args) {
+
+        int a = 12;
+        int b = 24;
+
+        int sum = a + b;
+        System.out.println(sum);
+    }
+}
