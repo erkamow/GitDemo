6f325fd0e6e13f5b8b48df7c7e00930ee1ef1a07
erkamow <68976455+erkamow@users.noreply.github.com>
2020-08-01 16:35:37 -0400

    Update happyB20.java

--- src/happyB20.java
+++ src/happyB20.java
@@ -6,5 +6,13 @@ public class happyB20 {
 
         int sum = a + b;
         System.out.println(sum);
+         System.out.println("================================================");
+        
+         int c = 13;
+        int d = 23;
+
+        int sum1 = c + d;
+        System.out.println(sum1);
+    }
     }
 }
